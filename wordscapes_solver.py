a3567a1bccbd02e388f19d4140f54ee47b4df312
rootshellz <>
2019-02-24 04:20:41 -0700

    Refactor to support direct calling and webapp

--- wordscapes_solver.py
+++ wordscapes_solver.py
@@ -3,17 +3,26 @@
 from itertools import permutations
 
 
+def solve(solvable, available_chars, word_list_file):
+    for letter in solvable:
+        if letter != "_": available_chars.remove(letter)
+    possible_word_candidates = make_all_possible_word_candidates(available_chars, solvable)
+    return check_possible_word_candidates_against_dictionary(possible_word_candidates, word_list_file)
+
+
 def get_input():
-    return input("Enter word to solve, using '_' or '-' for blanks (e.g. __Y_L): ").lower().replace("-","_"), sorted(input("Enter all the available letters, including ones already placed in the solvable (order does not matter) (e.g. LYSOLID): ").lower())
+    return input("Enter word to solve, using '_' or '-' for blanks (e.g. __Y_L): ").lower().replace("-","_"),\
+           sorted(input("Enter all the available letters, including ones already placed in the solvable (order does not matter) (e.g. LYSOLID): ").lower())
 
 
-def make_all_combos_from_availables():
-    return set(permutations(availables, len(blanks)))
+def make_all_combos_from_available_chars(available_chars, blanks):
+    return set(permutations(available_chars, len(blanks)))
 
 
-def make_all_possible_word_candidates():
+def make_all_possible_word_candidates(available_chars, solvable):
     possible_word_candidates = []
-    for combo in make_all_combos_from_availables():
+    blanks = [char for char in range(len(solvable)) if solvable[char] == "_"]
+    for combo in make_all_combos_from_available_chars(available_chars, blanks):
         possible_word_candidate = list(solvable)
         for index in range(len(combo)):
             possible_word_candidate[blanks[index]] = combo[index]
@@ -21,9 +30,9 @@ def make_all_possible_word_candidates():
     return possible_word_candidates
 
 
-def check_possible_word_candidates_against_dictionary():
-    # Word lists: https://github.com/dwyl/english-words 
-    with open("words_alpha.txt") as dict_file:
+def check_possible_word_candidates_against_dictionary(possible_word_candidates, word_list_file):
+    # Word lists: https://github.com/dwyl/english-words
+    with open(word_list_file) as dict_file:
         word_list = [word.strip().lower() for word in dict_file.readlines()]
     possible_words = []
     for possible_word in possible_word_candidates:
@@ -32,14 +41,7 @@ def check_possible_word_candidates_against_dictionary():
     return possible_words
 
 
-solvable, availables = get_input()
-
-for letter in solvable:
-    if letter != "_": availables.remove(letter)
-
-blanks = [char for char in range(len(solvable)) if solvable[char] == "_"]
-
-all_available_combos = make_all_combos_from_availables()
-possible_word_candidates = make_all_possible_word_candidates()
-for possible_word in check_possible_word_candidates_against_dictionary():
-    print(possible_word)
+if __name__ == "__main__":
+    input_solvable, input_available_chars = get_input()
+    for possible_word in solve(input_solvable, input_available_chars, word_list_file="words_alpha.txt"):
+        print(possible_word)
